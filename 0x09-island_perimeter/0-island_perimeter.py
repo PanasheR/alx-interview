f46d29aa341edef2646dbce9b090fa53213cde15
Panashe Rusakaniko <panasherussy10@gmail.com>
2022-06-13 00:45:43 +0200

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -14,16 +14,16 @@ def island_perimeter(grid):
     """
 
     perimeter = 0
-    grl = len(grid)
-    grw = len(grid[0])
+    l = len(grid)
+    w = len(grid[0])
 
-    for i in range(grl):
-        for j in range(grw):
+    for i in range(l):
+        for j in range(w):
             if grid[i][j] == 1:
                 for x, y in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
                     A, B = i + x, j + y
-                    #display
-                    if A >= grl or B >= grw or A < 0 or B < 0 or grid[A][B] == 0:
+                    # display
+                    if A >= l or B >= w or A < 0 or B < 0 or grid[A][B] == 0:
                         perimeter += 1
 
     return perimeter
